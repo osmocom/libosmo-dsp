59e29c2043cc8bbc14fc7f483aadea2a7b7d0f56
Sylvain Munaut <tnt@246tNt.com>
2013-06-16 22:48:59 +0200

    iqbal: Move the SDR# comment to the Doxygen file header
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- src/iqbal.c
+++ src/iqbal.c
@@ -28,6 +28,16 @@
 
 /*! \file iqbal.c
  *  \brief IQ balance utils implementation
+ *
+ *  The actual algorithm used for estimation of the imbalance and its
+ *  optimization is inspired by the IQ balancer of SDR# by Youssef Touil
+ *  and described here :
+ *
+ *  http://sdrsharp.com/index.php/automatic-iq-correction-algorithm
+ *
+ *  The main differences are:
+ *   - Objective function uses complex correlation of left/right side of FFT
+ *   - Optimization based on steepest gradient with dynamic step size
  */
 
 #include <complex.h>
@@ -206,17 +216,6 @@ osmo_iqbal_cxvec_estimate(const struct osmo_cxvec *sig,
 /* ------------------------------------------------------------------------ */
 /* IQ balance optimization                                                  */
 /* ------------------------------------------------------------------------ */
-
-/*
- * The actual algorithm used here is inspired by the IQ balancer of SDR#
- * by Youssef Touil and described here :
- *
- * http://sdrsharp.com/index.php/automatic-iq-correction-algorithm
- *
- * The main differences are:
- *  - Objective function uses complex correlation of left/right side of FFT
- *  - Optimization based on steepest gradient with dynamic step size
- */
 
 /*! \brief Default values for the optimization algorithm */
 const struct osmo_iqbal_opts osmo_iqbal_default_opts = {
